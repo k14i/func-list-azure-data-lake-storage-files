0a32a184ee7f0fa286b04013f4415b65e6aadaca
Keisuke Takahashi <k14i@outlook.com>
2023-02-13 10:30:11 +0900

    Fixed url encoded parameters support

--- listAzureDataLakeStorageFilesByHttpTrigger/__init__.py
+++ listAzureDataLakeStorageFilesByHttpTrigger/__init__.py
@@ -17,10 +17,18 @@ def _remove_quotes(param: str) -> str:
     return param
 
 def _decode_request_params(param) -> str:
-    if re.match(re.compile(r'^[A-Za-z0-9+/]+={0,2}$'), param) and b64encode(b64decode(param)) == param.encode('utf-8'):
-        param = b64decode(param).decode('utf-8')
-    if re.match(re.compile(r'(%[0-9a-fA-F]{2}|[^<>\'" %])+'), param) and quote(unquote(param)) == param:
-        param = unquote(param)
+    try:
+        if re.match(re.compile(r'^[A-Za-z0-9+/]+={0,2}$'), param) and b64encode(b64decode(param)) == param.encode('utf-8'):
+            param = b64decode(param).decode('utf-8')
+    except Exception:
+        pass
+
+    try:
+        if re.match(re.compile(r'(%[0-9a-fA-F]{2}|[^<>\'" %])+'), param) and quote(unquote(param)) == param:
+            param = unquote(param)
+    except Exception:
+        pass
+    
     return param
 
 
